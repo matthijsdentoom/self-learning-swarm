5643bbc1f27fe9890b1ff40a1558604500fee222
matthijs <m.denToom@student.tudelft.nl>
2018-12-12 15:40:30 +0100

    Automated file location

--- robot_learner/my_openai_neat.py
+++ robot_learner/my_openai_neat.py
@@ -1,14 +1,20 @@
 import copy
+import shutil
+
 import gym
 import scrimmage.utils
 import neat
 import csv
 import visualize
+from os.path import expanduser
 
 max_evaluation_steps = 2000
-num_generations = 1
-metric_directory = "/home/matthijs/Documents/scrimmage-logs/flock-experiment/latest/"   #TODO, only works single-threaded
+num_generations = 150
+
+log_directory = expanduser("~") + "/Documents/scrimmage-logs/"
+experiment_directory = log_directory + "/flock-experiment"
 num_robots = 10
+
 
 def get_action(nn, observation):
     """Returns the action which the robot does with the current observation"""
@@ -16,6 +22,7 @@ def get_action(nn, observation):
     actions = nn.activate(observation)
 
     return actions
+
 
 def execute_environment(net, mission_file_name):
 
@@ -34,7 +41,7 @@ def execute_environment(net, mission_file_name):
         obs, reward, done = env.step(actions)[:3]
 
         if done and not i == max_evaluation_steps - 1:
-            total_reward = execute_environment(net, env)  # Execute the problem again
+            total_reward = execute_environment(net, mission_file_name)  # Execute the problem again
             # TODO: prevent this from going in infinite recursion.
 
             return total_reward
@@ -42,7 +49,9 @@ def execute_environment(net, mission_file_name):
     env.close()
 
     total_reward = 0
-    with open(metric_directory + 'summary.csv', mode='r') as csv_file:
+
+    # TODO, only works single-threaded, because using latest
+    with open(experiment_directory + '/latest/summary.csv', mode='r') as csv_file:
         csv_reader = csv.DictReader(csv_file)
         for row in csv_reader:
             # Assume only one row is there with the score.
@@ -51,17 +60,19 @@ def execute_environment(net, mission_file_name):
     return total_reward
 
 
-def eval_genomes(genomes, config, env):
+def eval_genomes(genomes, config, mission_file_name):
     """This function evaluates the current genomes in the population."""
     for genome_id, genome in genomes:
         genome.fitness = 0
         net = neat.nn.FeedForwardNetwork.create(genome, config)
-        genome.fitness = execute_environment(net, env)
+        genome.fitness = execute_environment(net, mission_file_name)
+
 
 def remove_scrimmage_env():
 
     if 'scrimmage-v0' in gym.envs.registry.env_specs.keys():
         del gym.envs.registry.env_specs['scrimmage-v0']
+
 
 def create_scrimmage_env(mission_file_name):
     """Create the scrimmage environment of the specified mission file."""
@@ -86,7 +97,7 @@ def create_scrimmage_env(mission_file_name):
     return env
 
 
-def learn_staying_on_line(env, config_path):
+def learn_flocking(mission_file_name, config_path):
     # Load configuration.
     config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
                          neat.DefaultSpeciesSet, neat.DefaultStagnation,
@@ -101,7 +112,7 @@ def learn_staying_on_line(env, config_path):
     p.add_reporter(stats)
 
     # Run for up for the given number of generations
-    f = lambda genomes, config: eval_genomes(genomes, config, env=mission_file_name)
+    f = lambda genomes, config: eval_genomes(genomes, config, mission_file_name=mission_file_name)
     winner = p.run(f, num_generations)
 
     winner_net = neat.nn.FeedForwardNetwork.create(winner, config)
@@ -116,16 +127,17 @@ def learn_staying_on_line(env, config_path):
 
     print(winner)
 
+    # Remove the experimental directory, since it is not used anymore.
+    shutil.rmtree(experiment_directory)
 
-def test_openai(mission_file_name):
-    env = create_scrimmage_env(mission_file_name)
 
-    learn_staying_on_line(env, 'config-feedforward')
+def main():
 
-    env.close()
+    mission_file_name = 'zebros_flocking.xml'
+
+    learn_flocking(mission_file_name, 'config-feedforward')
 
 
 if __name__ == '__main__':
-    mission_file_name = 'zebros_flocking.xml'
 
-    test_openai(mission_file_name)
+    main()
