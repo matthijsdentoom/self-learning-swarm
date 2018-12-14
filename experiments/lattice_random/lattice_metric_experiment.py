9b56e03b83b81418a9fa3fee681d06cd2208040f
matthijs <m.denToom@student.tudelft.nl>
2018-12-14 21:13:36 +0100

    Fixed bug in neural network flocking mechanism.

--- experiments/lattice_random/lattice_metric_experiment.py
+++ experiments/lattice_random/lattice_metric_experiment.py
@@ -8,16 +8,17 @@ from gym_manager import GymScrimmageEnvironment
 from neat_learner import NeatLearner
 
 max_evaluation_steps = 2000
-num_generations = 150
+num_generations = 1
 
 log_directory = expanduser("~") + "/Documents/scrimmage-logs/"
 experiment_directory = log_directory + "flock-experiment"
 final_directory = log_directory + "flock-experiment-final"
 
-experiment_mission_file = "zebros_flocking.xml"
-output_mission_file = "zebros_flocking_output.xml"
+experiment_mission_file = "zebros_flocking_lattice.xml"
+output_mission_file = "zebros_flocking_lattice_output.xml"
 
-def main():
+
+def lattice_metric_experiment():
 
     experiment_manager = GymScrimmageEnvironment(experiment_mission_file, max_evaluation_steps)
     neat_learner = NeatLearner('config-feedforward', experiment_directory)
@@ -25,6 +26,8 @@ def main():
     neat_learner.run_experiment(num_generations, experiment_manager)
 
     experiment_manager.close()
+
+    print("Running winner")
 
     # Print the winner.
     experiment_manager = GymScrimmageEnvironment(output_mission_file, max_evaluation_steps)
@@ -36,4 +39,4 @@ def main():
 
 if __name__ == '__main__':
 
-    main()
+    lattice_metric_experiment()
